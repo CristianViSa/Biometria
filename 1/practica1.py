0e3b6aae00f292675d4f553188f5bbb6d61863bc
CristianViSa <cristianvillarroya1234@gmail.com>
2021-04-27 22:40:57 +0200

    Test AUC

--- 1/practica1.py
+++ 1/practica1.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import scipy.stats as stats
+from sklearn.metrics import roc_auc_score
+
 
 DATA_PATH = "data/"
 
@@ -19,7 +22,12 @@ def auc():
 def roc(scores, positives, negatives, plot=True):
     values = [val[0] for val in scores]
     labels = [lab[1] for lab in scores]
-
+    test = roc_auc_score(labels, values)
+    n1 = positives
+    n0 = len(scores) - n1
+    print("AUC : ")
+    print(test)
+    print(1 - 0.8831634391249776)
     fpr = []
     tpr = []
     thresholds = []
@@ -60,6 +68,17 @@ def roc(scores, positives, negatives, plot=True):
 if __name__ == '__main__':
     scores_c = read_file(DATA_PATH + "scoresA_clientes", 1)
     scores_i = read_file(DATA_PATH + "scoresA_impostores", 0)
+    values1 = [val[0] for val in scores_c]
+    values2 = [val[0] for val in scores_i]
+    u_statistic, pVal = stats.mannwhitneyu(values1, values2)
+    multi = len(values1) * len(values2)
+    # Print results
+
+    print('P value:')
+    print(pVal)
+    print('EL AUC QUE PARECE SER')
+    print(u_statistic /multi)
     scores = scores_i + scores_c
     scores = sorted(scores)
     roc(scores, len(scores_c), len(scores_i))
+
