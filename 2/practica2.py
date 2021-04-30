3dd5363a5eda09767239ef45ba1b16c842a2424d
Cristian <cristianvillarroya1234@gmail.com>
2021-04-30 11:46:19 +0200

    Added PCA with SKLEARN library

--- 2/practica2.py
+++ 2/practica2.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
 
 TRAIN_DATA_PATH = "data/Train/"
 TEST_DATA_PATH = "data/Test"
@@ -57,4 +58,36 @@ def read_images(dir):
 if __name__ == '__main__':
     train_images, train_labels = read_images(TRAIN_DATA_PATH)
     test_images, test_labels = read_images(TEST_DATA_PATH)
+    print(train_images.shape)
+    #pca = PCA().fit(train_images)
 
+    # Ver matriz con la varianza
+    # plt.figure(figsize=(18, 7))
+    # plt.plot(pca.explained_variance_ratio_.cumsum())
+    # print(np.where(pca.explained_variance_ratio_.cumsum() > 0.95))
+    # plt.show()
+    # Comprobar donde la energia (varianza) es mayor de un 95
+    # PCA CON SKLEARN
+    # results = []
+    # variance_sum = []
+    # for i in range(1, 100):
+    #     pca = PCA(n_components=i)
+    #     tr_img = pca.fit_transform(train_images)
+    #
+    #     variance_sum = pca.explained_variance_ratio_.cumsum()
+    #     tst_img = pca.transform(test_images)
+    #     clf = KNeighborsClassifier(n_neighbors = 1)
+    #     clf.fit(tr_img, train_labels)
+    #     results.append(clf.score(tst_img, test_labels))
+    #     pca.
+    # print(results)
+    # print(np.where(variance_sum > 0.95))
+    # plt.plot(list(range(1, 200)), results)
+    # plt.xlabel("dimensiones")
+    # plt.ylabel("accuracy")
+    # plt.plot()
+    # results = np.array(results)
+    # print("Best result ", np.amax(results), "with " , np.argmax(results), " components")
+    # n = 200
+    # d = 100x100 casi
+    # Truco --> No calcular diagonalizacion Cdxd se calcula C`nxn
